d3a5313c5c868e608c5877225f96f0e68567dcfd
iamsabet <iamsabet@gmail.com>
2023-08-05 22:32:06 +0330

    checkpoint

--- client/src/canvas/index.tsx
+++ client/src/canvas/index.tsx
@@ -4,17 +4,35 @@ import { Environment, Center } from "@react-three/drei";
 import Shirt from "./Shirt";
 import BackDrop from "./BackDrop";
 import CameraRig from "./CameraRig";
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const CanvasModel = ({ canvasId, canvasType }: CanvasType) => {
+  const [initial, setinitial] = useState(canvasType === "close");
+  // its a bad work ... but lets keep it for now
+  useEffect(() => {
+    // if (canvasType === "close") {
+    // console.log("before" + new Date());
+    setTimeout(() => {
+      if (!initial) setinitial((prev) => !prev);
+      // console.log("after : " + new Date());
+    }, 500);
+    // }
+    return () => {};
+  }, []);
   const bgColor = canvasType === "open" ? "#F5F3EF" : "#E7E2DA";
   return (
     <>
+      {/* {!initial && (
+        <div
+          className="absolute w-full h-full opacity-100 z-10 transition-all duration-500 ease-linear"
+          style={{ background: "#ffffff" }}
+        ></div>
+      )} */}
       <Canvas
         shadows
-        // resize={{
-        //   debounce: !initial ? Infinity : 10,
-        // }}
+        resize={{
+          debounce: !initial ? Infinity : 10,
+        }}
         camera={{ position: [0, 0, 3], fov: 27 }}
         gl={{
           preserveDrawingBuffer: canvasType === "open",
